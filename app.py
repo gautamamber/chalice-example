d4751537ae393c1699776ef3b14ce58e7e028842
gautamamber <ambergautam1@gmail.com>
2019-10-01 23:23:14 +0530

    feat():url parameters

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ app = Chalice(app_name='helloworld')
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+# url parameters
+
+CITIES_OF_STATE = {
+    "seattle": "WA",
+    "portland": "OR"
+}
+
+@app.route('/cities/{city}')
+def state_of_city(city):
+    data = {
+        "state": CITIES_OF_STATE[city]
+    }
+    return data
