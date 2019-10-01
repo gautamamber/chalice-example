fe90c1f1728e4076984ba42e992472beac09e80d
gautamamber <ambergautam1@gmail.com>
2019-10-01 23:44:13 +0530

    feat():handle requests from event

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from chalice import Chalice
-from chalice import BadRequestError
+from chalice import BadRequestError, NotFoundError
 
 app = Chalice(app_name='helloworld')
 app.debug = True
-
+OBJECTS = {}
 @app.route('/')
 def index():
     return {'hello': 'world'}
@@ -22,9 +22,23 @@ def state_of_city(city):
     except KeyError:
         raise BadRequestError("Unknown city '%s', valid choices are: %s" % (city, ','.join(CITIES_OF_STATE.keys())))
 
-@app.route('/resource/{value}', methods=['PUT'])
+# url put or post methods
+@app.route('/resource/{value}', methods=['PUT', 'POST'])
 def put_test(value):
     data = {
         "value" : value
     }
     return data
+
+# Request meta data
+@app.route('/objects/{key}', methods = ['GET', 'PUT'])
+def myobject(key):
+    request = app.current_request
+    print(request.json_body)
+    if request.method == 'PUT':
+        OBJECTS[key] = request.json_body
+    elif request.method == 'GET':
+        try:
+            return {key: OBJECTS[key]}
+        except KeyError:
+            raise NotFoundError(key)
